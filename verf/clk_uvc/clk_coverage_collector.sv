98f63c3dd7abf7ec9eef31732965f3e3ddc3cfaf
mohamedtareq24 <1900889@eng.asu.edu.eg>
2026-02-24 22:01:50 +0200

    Fix incorrect bin type in clk coverage collector: change illegal_bins to ignore_bins

--- verf/clk_uvc/clk_coverage_collector.sv
+++ verf/clk_uvc/clk_coverage_collector.sv
@@ -8,14 +8,14 @@ class clk_coverage_collector extends uvm_subscriber #(clk_mon_tr);
             bins max_bin     = {4020, 4021};
             bins min_bin     = {3999};
             bins others_bin  = default;
-            illegal_bins out_of_range = {[0 : 3998] , [4022 : $]};
+            ignore_bins out_of_range = {[0 : 3998] , [4022 : $]};
         }
         
         cdr_period_cp: coverpoint tr.cdr_period{
             bins max_bin     = {4020 , 4021};
             bins min_bin     = {3999};
             bins others_bin  = default;
-            illegal_bins out_of_range = {[0 : 3998] , [4022 : $]} ;
+            ignore_bins out_of_range = {[0 : 3998] , [4022 : $]} ;
         }
 
         cross sys_period_cp, cdr_period_cp {
