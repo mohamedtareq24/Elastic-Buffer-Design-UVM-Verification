73779c3e9376cecaa98deb755f34c2cd933ad06f
mohamedtareq24 <1900889@eng.asu.edu.eg>
2026-02-24 22:05:18 +0200

    ScoreBoard proves data integrity

--- verf/eb_env/eb_scoreboard.sv
+++ verf/eb_env/eb_scoreboard.sv
@@ -10,16 +10,19 @@ class eb_scoreboard extends uvm_component;
   uvm_tlm_analysis_fifo#(rd_mon_tr) rd_fifo;
 
   bit [19:0] expected_payload_q[$];
-  int unsigned skp_count_inserted;
-  int unsigned skp_count_dropped;
   int unsigned num_mismatches;
   int unsigned num_matches;
+  int unsigned skp_count_seen_wr;
+  int unsigned skp_count_seen_rd;
+
 
 
   function new(string name = "eb_scoreboard", uvm_component parent = null);
     super.new(name, parent);
     wr_fifo = new("wr_fifo", this);
     rd_fifo = new("rd_fifo", this);
+    skp_count_seen_wr = 0;
+    skp_count_seen_rd = 0;
   endfunction
 
   virtual function void build_phase(uvm_phase phase);
@@ -37,58 +40,47 @@ class eb_scoreboard extends uvm_component;
     wr_item tr;
     forever begin
       wr_fifo.get(tr);
-      if (expected_payload_q.size() > cfg.cor_max) begin
-        if (is_skp(tr.data)) begin
-          // DO NOT add SKP to expected payload queue.
-          `uvm_info("EBSB", $sformatf("SKP Dropped. Size is %0d." , expected_payload_q.size()), UVM_MEDIUM)
-          skp_count_dropped++;
-          /*
-          raise an event for the dropped SKP
-          */
-        end
-      else
+      if (is_skp(tr.data)) begin
+        skp_count_seen_wr++;
+        `uvm_info("EBSB", $sformatf("SKP observed on write: 'h%05h", tr.data), UVM_HIGH)
+      end
+      else begin
         expected_payload_q.push_back(tr.data);
+        `uvm_info("EBSB", $sformatf("Data added to expected payload queue: 'h%05h. Size is %0d." , tr.data, expected_payload_q.size()), UVM_HIGH )
       end
     end
   endtask
 
   function bit [19:0] genrate_ref_rd();
     bit [19:0] ref_rd_data ;
-    if (expected_payload_q.size() < cfg.cor_min) begin
-        if (is_skp(ref_rd_data)) begin
-          // Don't pop from the reference queue
-          ref_rd_data = expected_payload_q[0]; // SKP value
-          skp_count_inserted++;
-          /*
-          raise an event for the inserted SKP
-          */
-          `uvm_info("EBSB", $sformatf("SKP Inserted. Size is %0d." , expected_payload_q.size()), UVM_MEDIUM)
+    
+    if (is_skp(expected_payload_q[0])) begin
+      if (expected_payload_q.size() <= cfg.cor_min ) begin
+          ref_rd_data = expected_payload_q[0];
         end
-        else begin
-          // Pop from the reference queue
-          ref_rd_data = expected_payload_q.pop_front();
-        end
-        return ref_rd_data;
     end
+    else
+    begin
+      ref_rd_data = expected_payload_q.pop_front();
+    end
+    return ref_rd_data;
   endfunction
 
-  /*
-  On receiving a read transaction, compare it against the reference model output.
-  Reference model output is generated based on the expected payload queue and the current COR_MIN setting.
-  using genrate_ref_rd() function 
-  */
   task automatic check_rd();
     rd_mon_tr tr;
-    bit [19:0] ref_rd_data ;
+    bit [19:0] ref_rd_data;
     forever begin
       rd_fifo.get(tr);
       num_tr++;
-
-      ref_rd_data = genrate_ref_rd();
-
+      if (is_skp(tr.data)) begin
+        skp_count_seen_rd++;
+        `uvm_info("EBSB", $sformatf("SKP observed on read: 'h%05h", tr.data), UVM_HIGH)
+        continue; // Don't compare SKP values, as they may be inserted by the DUT
+      end
+      else
+        ref_rd_data = genrate_ref_rd();
       if (expected_payload_q.size() == 0) begin
         `uvm_error("EBSB", $sformatf("Unexpected payload on output: 'h%05h", tr.data))
-        // Wherer Did this data come from?
       end
 
       if (tr.data !== ref_rd_data) begin
@@ -113,6 +105,9 @@ class eb_scoreboard extends uvm_component;
     `uvm_info("EBSB", $sformatf("Total transactions processed: %0d", num_tr), UVM_LOW)
     `uvm_info("EBSB", $sformatf("Total mismatches detected: %0d", num_mismatches), UVM_LOW)
     `uvm_info("EBSB", $sformatf("Total matches detected: %0d", num_matches), UVM_LOW)
+    `uvm_info("EBSB", $sformatf("Total SKPs seen on write side: %0d", skp_count_seen_wr), UVM_LOW)
+    `uvm_info("EBSB", $sformatf("Total SKPs seen on read side: %0d", skp_count_seen_rd), UVM_LOW)
+    `uvm_info("EBSB", $sformatf("Percentage of SKPs in the stream : %0.2f%%", (skp_count_seen_wr*100.0)/num_tr), UVM_LOW)
   endfunction
 endclass
 
