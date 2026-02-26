09be1ce4605aad6417d9749e8ba3380162052a75
mohamedtareq24 <1900889@eng.asu.edu.eg>
2026-02-27 00:57:53 +0200

    Enhance eb_scoreboard: track packet sizes

--- verf/eb_env/eb_scoreboard.sv
+++ verf/eb_env/eb_scoreboard.sv
@@ -14,7 +14,7 @@ class eb_scoreboard extends uvm_component;
   int unsigned num_matches;
   int unsigned skp_count_seen_wr;
   int unsigned skp_count_seen_rd;
-
+  int pkt_size_q[$];
 
 
   function new(string name = "eb_scoreboard", uvm_component parent = null);
@@ -38,26 +38,47 @@ class eb_scoreboard extends uvm_component;
 
   task automatic collect_wr();
     wr_item tr;
+    static bit pkt_start = 0;
+    static int pkt_size = 0;
     forever begin
       wr_fifo.get(tr);
       if (is_skp(tr.data)) begin
         skp_count_seen_wr++;
         `uvm_info("EBSB", $sformatf("SKP observed on write: 'h%05h", tr.data), UVM_HIGH)
+        pkt_start = !pkt_start; // Toggle packet start on each SKP
       end
       else begin
         expected_payload_q.push_back(tr.data);
         `uvm_info("EBSB", $sformatf("Data added to expected payload queue: 'h%05h. Size is %0d." , tr.data, expected_payload_q.size()), UVM_HIGH )
+      end
+      if (pkt_start) begin
+        pkt_size++;
+      end
+      if (pkt_size && !pkt_start) begin
+        pkt_size_q.push_back(pkt_size);
+        `uvm_info("EBSB", $sformatf("Packet ended. Size was %0d. Recorded in packet size queue.", pkt_size), UVM_HIGH)
+        pkt_size = 0;
       end
     end
   endtask
 
   function bit [19:0] genrate_ref_rd();
     bit [19:0] ref_rd_data ;
-    
+    static bit [19:0] last_rd_data;
+
+    if (expected_payload_q.size() == 1) begin
+    last_rd_data = expected_payload_q[0];
+    `uvm_info("EBSB", $sformatf("Only one item left in expected payload queue: 'h%05h", last_rd_data), UVM_LOW)
+    end
+
     if (is_skp(expected_payload_q[0])) begin
-      if (expected_payload_q.size() <= cfg.cor_min ) begin
+      if (expected_payload_q.size() < cfg.cor_min ) begin
           ref_rd_data = expected_payload_q[0];
         end
+    end
+    else if (expected_payload_q.size() == 0) begin
+      `uvm_warning("EBSB", $sformatf("Expected payload queue is empty. expected data is %h.", last_rd_data))
+      ref_rd_data =last_rd_data; 
     end
     else
     begin
@@ -80,7 +101,7 @@ class eb_scoreboard extends uvm_component;
       else
         ref_rd_data = genrate_ref_rd();
       if (expected_payload_q.size() == 0) begin
-        `uvm_error("EBSB", $sformatf("Unexpected payload on output: 'h%05h", tr.data))
+        `uvm_warning("EBSB", $sformatf("FIFO is empty, Read Data='h%05h", tr.data))
       end
 
       if (tr.data !== ref_rd_data) begin
@@ -108,6 +129,7 @@ class eb_scoreboard extends uvm_component;
     `uvm_info("EBSB", $sformatf("Total SKPs seen on write side: %0d", skp_count_seen_wr), UVM_LOW)
     `uvm_info("EBSB", $sformatf("Total SKPs seen on read side: %0d", skp_count_seen_rd), UVM_LOW)
     `uvm_info("EBSB", $sformatf("Percentage of SKPs in the stream : %0.2f%%", (skp_count_seen_wr*100.0)/num_tr), UVM_LOW)
+    `uvm_info("EBSB" , $sformatf("Max Packet Size: %p Min Packet Size: %p", pkt_size_q.max(), pkt_size_q.min()), UVM_LOW)
   endfunction
 endclass
 
